a25160e09b2114c412dc896b1f5b5cec132cf0f3
abinand <abinandsathyan532gmail.com>
2023-10-24 13:57:21 +0530

    host cahnge

--- productlist/src/axios/axios.js
+++ productlist/src/axios/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    // baseURL:'https://jobport.onrender.com/',
-    baseURL:'http://localhost:3000/',
+    baseURL:'https://prodectlist.onrender.com/',
+    // baseURL:'http://localhost:3000/',
     headers: {
         "Content-Type": "application/json",
       }
